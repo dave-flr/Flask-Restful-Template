fd11b0d4b8ad18b5efe445f1dc069e6b37985f65
daveflr <daveflr@protonmail.com>
2021-04-16 16:27:36 -0600

    Enabling CORS

--- app/api.py
+++ app/api.py
@@ -2,6 +2,7 @@ import os
 
 from dotenv import load_dotenv
 from flask import Flask
+from flask_cors import CORS
 from flask_restful import Api
 
 from app.config.app import DevelopmentConfig, ProductionConfig
@@ -17,3 +18,6 @@ api = Api(app)
 # Loading config
 config = ProductionConfig() if os.getenv('FLASK_ENV') == 'development' else DevelopmentConfig()
 app.config.from_object(config)
+
+# Modules
+CORS(app)
